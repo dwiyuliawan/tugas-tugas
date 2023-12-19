bffd3935b0225c679cf17449d146635f7975989b
dwiyuliawan <dwiyuliawan25@gmail.com>
2023-12-19 12:59:47 +0700

    tugas rumus php

---
+++ tugas_php/bangun_datar.php
@@ -0,0 +1,31 @@
+<?php
+
+    $s = 5;
+    $p = 7;
+    $l = 8;
+    $a = 6;
+    $t = 4;
+    $j = 10;
+
+    //1. luas persegi = s*s
+    $luas_persegi = $s*$s;
+    echo "$s*$s = $luas_persegi";
+    echo "<hr>";
+    //2. luas persegi panjang = p*l
+    $luas_persegi_panjang = $p*$l;
+    echo "$p*$l = $luas_persegi_panjang";
+    echo "<hr>";
+    //3. luas segi tiga = 1/2*a*t
+    $luas_segi_tiga = 1/2*$a*$t;
+    echo "1/2*$a*$t = $luas_segi_tiga";
+    echo "<hr>";
+    //4. luas lingkaran = 22/7*j*j
+    $luas_lingkaran = 22/7*$j*$j;
+    echo "22/7*$j*$j = $luas_lingkaran";
+    echo "<hr>";
+    //5. luas jajar genjang = a*t
+    $luas_jajar_genjang = $a*$t;
+    echo "$a*$t = $luas_jajar_genjang";
+    echo "<hr>";
+
+?>
---
+++ tugas_php/bangun_ruang.php
@@ -0,0 +1,19 @@
+<?php
+    $s = 9;
+    $l = 8;
+    $t = 7;
+    $p = 5;
+
+    //1. volum kubus = s*s*s
+    $volume_kubus = $s*$s*$s;
+    echo "$s*$s*$s = $volume_kubus";
+    echo "<hr>";
+    //2. volum limas = 1/3*l*t
+    $volume_limas = 1/3*$l*$t;
+    echo "1/3*$l*$t = $volume_limas";
+    echo "<hr>";
+    //3. volume balok = p*l*t
+    $volume_balok = $p*$l*$t;
+    echo "$p*$l*$t = $volume_balok";
+    echo "<hr>";
+?>
